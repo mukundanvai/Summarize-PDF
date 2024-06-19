c6c384e33b6001605b5cc781dc7f07411a5f4edc
mukundanvai <168465726+mukundanvai@users.noreply.github.com>
2024-06-19 17:18:14 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from crewai import Agent, Task
+from crewai import Crew, Agent, Task, Process
 from langchain_openai import ChatOpenAI
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import tiktoken, os
 from dotenv import load_dotenv
-import streamlit as st
 
 load_dotenv()
 
@@ -21,50 +20,86 @@ class SummarizationCrew():
     self.text = text
 
   def agents_tasks(text):
-    agent = Agent(
-      role='Principal Researcher',
-      goal='Do amazing research and summaries on the {text} you are working with. Do not report anything verbatim',
-      backstory="You're a Principal Summarizer at a big company and you need to summarize about a given topic.",
+    summary_agent = Agent(
+      role='Principal Summarizer',
+      goal='Do summaries on the {text} you are working with. Do not report anything verbatim. DO NOT report any information not provided in the text.',
+      backstory="You're a Principal Summarizer at a big company and you need to summarize the given text.",
       llm=llm,
       verbose=True,
-      allow_delegation=False
+      allow_delegation=False,
+      memory=True
       )
-    task = Task(
-      agent=agent,
-      description=f'Analyze and summarize the {text} below, make sure to include the most relevant information in the summary, return only the summary nothing else.\
-      Do not include information that is not important. Ensure you summarize the quotes and do not report anything verbatim.\n\nCONTENT\n----------\n{text}',
-      expected_output= "Summary of the document"
+    summary_task = Task(
+      agent=summary_agent,
+      description=f'Summarize only the {text} given, make sure to include the most relevant information given in the text in the summary, return only the summary nothing else.\
+      DO NOT make up information. DO NOT repeat information. Ensure you summarize the quotes and do not report anything verbatim.\
+      Do not report any information not present in the {text}. Dates and locations mentioned in the {text} are important, do include it in the report.\
+      Do not mention if something is not present in the text. Ensure you are gramatically correct\n\nCONTENT\n----------\n{text}',
+      expected_output= "Final summary of the text should include only the contents provided in the text"
+      )
+    
+    verify_agent = Agent(
+      role='Principal Grammar Checker',
+      goal='Verify if the contents produced by the {summary_agent} are gramatically correct. Do not report anything verbatim. DO NOT make up information.',
+      backstory="You're an expert Principal Grammar verfier at a big company and you need to verify the grammar in a given text.",
+      llm=llm,
+      verbose=True,
+      allow_delegation=True,
+      memory=False
       )
 
-    summary = task.execute()
+    verify_task = Task(
+      agent=verify_agent,
+      description=f'Verify if the {text} provided and the contents produced by the summary agent is gramatically correct. If not then call the summary agent to do its task again.\
+      Ensure that the contents produced are gramatically correct. DO NOT make up information. DO NOT report anything not presented by the summary agent.',
+      expected_output="Verified summary of the text provided"
+      )
+    
+    crew = Crew(
+      agents=[summary_agent],
+      tasks=[summary_task],
+      process=Process.sequential
+    )
+    summary = crew.kickoff()
     return summary
 
   def run(self):
-
     text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=round(len(self.text)/4),
+    chunk_size=round(len(self.text)/2),
     chunk_overlap=0,
     )
     text_list = text_splitter.split_text(self.text)
+    text = self.text
 
     encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
     final_output = []
-    n = 0
-    while n < len(text_list):
-      if len(encoding.encode(text_list[n])) <= 5000:
-        final_output.append(SummarizationCrew.agents_tasks(text=text_list[n]))
-        n += 1
-      else:
-        text_splitter = RecursiveCharacterTextSplitter(
+    i, n = (0, 0)
+    text_splitter = RecursiveCharacterTextSplitter(
         separators=' ',
         chunk_size=5000,
         chunk_overlap=500,
         length_function=len,
           )
-        chunks = text_splitter.split_text(text=text_list[n])
+    while n < len(text_list):
+      if len(encoding.encode(text_list[n])) <= 5000 and i==0:
+        final_output.append(SummarizationCrew.agents_tasks(text=text_list[n]))
+        n += 1
+        i = 0
+      elif len(encoding.encode(text=text)) <= 5000:
+        final_output.append(SummarizationCrew.agents_tasks(text=text))
+        n += 1
+        i = 0
+      else:
+        if i == 0:
+          chunks = text_splitter.split_text(text=text_list[n])
+        else:
+          chunks = text_splitter.split_text(text=text) 
         summaries = []
         for chunk in chunks:
           summary = SummarizationCrew.agents_tasks(text=chunk)
           summaries.append(summary)
         text = " ".join(summaries)
+        i = 1
     return "\n\n".join(final_output)
+
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -6,18 +6,32 @@ def main():
     st.header("Agent PDF Summarizer")
     pdf_file = st.file_uploader("Upload PDF File", type="pdf", accept_multiple_files=False)
     submit = st.button("Generate Summary")
+    summary = []
+    if pdf_file and submit:    
+        # Create a PDF reader object
+        pdf_reader = PdfReader(pdf_file)
 
-    if pdf_file and submit:
-        reader = PdfReader(pdf_file)
-        text = ""
-        for page in reader.pages:
-            text += page.extract_text()
+        # Get the number of pages in the PDF
+        num_pages = len(pdf_reader.pages)
 
-        summary_crew = SummarizationCrew(text=text)
-        summary = summary_crew.run()
+        # Loop through each page and extract the text
+        for page_num in range(num_pages):
+            # Get the page object
+            page = pdf_reader.pages[page_num]
+            
+            # Extract the text from the page
+            text = page.extract_text()
+            
+            # Print the text
+            page_summary = SummarizationCrew.agents_tasks(text=text)
+            summary.append(page_summary)
+        text = "\n\n".join(summary)
+        final_summary_obj = SummarizationCrew(text=text)
+        final_summary = final_summary_obj.run()
+        st.write(final_summary)
 
-        st.write("Summary:")
-        st.write(summary)
+        # Close the PDF file
+        pdf_file.close()
 
 if __name__ == "__main__":
     main()
